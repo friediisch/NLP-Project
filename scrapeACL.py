0f47e9193e7e8ebe484221ab4c451596e93e6cae
pkurzend1 <p@pdku.de>
2018-12-20 17:42:07 +0100

    scraper for ACL papers

--- scrapeACL.py
+++ scrapeACL.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 
+
+path = ".\\data\ACL\\"
 url = 'http://aclweb.org/anthology/'
 html = urlopen(url).read()
 
@@ -19,32 +21,51 @@ L = ['J,', 'Q,', 'P,', 'E,', 'N,', 'D,', 'K,', 'S,', 'W,', 'A,', 'C,', 'H,', 'L,
 
 
 pattern_old = r'http:\/\/aclweb\.org\/anthology\/[JQPENDKSWACHLYOT]\/[JQPENDKSWACHLYOT]\d\d\/'
-pattern = r'[JQPENDKSWACHLYOT]\/[JQPENDKSWACHLYOT]\d\d\/'
-
+pattern = r'[JQPENDKSWACHLYTUX]\/[JQPENDKSWACHLYTUX]\d\d\/'
 tags = soup.findAll('a', href = re.compile(pattern))
 links = []
-
 for tag in tags:
     print(url + tag.attrs['href'])
     links.append(url + tag.attrs['href'])
 
 
-def get_pdf(links):
+def download_file(download_url, name):
+    response = urlopen(download_url)
+    file = open(path + name, 'wb')
+    file.write(response.read())
+    file.close()
+
+def get_pdf(links, baseURL=None):
+    
+        
     pdf_papers = []
     for link in links:
+        if baseURL is None:
+            baseURL = link
         html = urlopen(link).read()
         soup = BeautifulSoup(html, 'html.parser')
         pattern = r'\.pdf$'
         tags = soup.findAll('a', href = re.compile(pattern))
         
         for tag in tags:
-            pdf_papers.append(link + tag.attrs['href'])
+            
+            print(baseURL.replace('.html', '/') + tag.attrs['href'])
+            pdf_papers.append(baseURL + tag.attrs['href'])
+            download_file(baseURL.replace('.html', '/') + tag.attrs['href'], tag.attrs['href'].replace('/', ''))
     return pdf_papers
+  
 
 pdfs = get_pdf(links)
+
+
+
 
 endings = ['ANN', 'BIOMED', 'DAT', 'DIAL', 'FSM', 'GEN', 'HAN', 'HUM', 'LEX', 'MEDIA', 'MOL', 'MT', 'NLL', 'PARSE', 'MORPHON', 'SEM', 'SLAV', 'SEMITIC', 'SLPAT', 'UR', 'WAC']
 endings = ['sig' + item.lower() for item in endings]
 
-
+links = []
+for ending in endings:
+    link = url + ending + '.html'
+    links.append(link)
+pdfs = get_pdf(links, url)
 
