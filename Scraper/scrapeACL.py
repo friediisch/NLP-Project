5c25f11714f4da49e2a19f65ca672b98d8ea68ee
friediisch <friedii.schestag@googlemail.com>
2019-01-22 18:57:29 +0100

    reordered everything

---
+++ Scraper/scrapeACL.py
@@ -0,0 +1,73 @@
+import bs4
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+
+
+path = ".\\data\ACL\\"
+url = 'http://aclweb.org/anthology/'
+html = urlopen(url).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+
+
+
+
+
+
+L = ['J,', 'Q,', 'P,', 'E,', 'N,', 'D,', 'K,', 'S,', 'W,', 'A,', 'C,', 'H,', 'L,', 'Y,', 'O,', 'T']
+
+
+
+pattern_old = r'http:\/\/aclweb\.org\/anthology\/[JQPENDKSWACHLYOT]\/[JQPENDKSWACHLYOT]\d\d\/'
+pattern = r'[JQPENDKSWACHLYTUX]\/[JQPENDKSWACHLYTUX]\d\d\/'
+tags = soup.findAll('a', href = re.compile(pattern))
+links = []
+for tag in tags:
+    print(url + tag.attrs['href'])
+    links.append(url + tag.attrs['href'])
+
+
+def download_file(download_url, name):
+    response = urlopen(download_url)
+    file = open(path + name, 'wb')
+    file.write(response.read())
+    file.close()
+
+def get_pdf(links, baseURL=None):
+    count = 0
+        
+    pdf_papers = []
+    for link in links:
+        if baseURL is None:
+            baseURL = link
+        html = urlopen(link).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        pattern = r'\.pdf$'
+        tags = soup.findAll('a', href = re.compile(pattern))
+        
+        for tag in tags:
+            count += 1
+            print(baseURL.replace('.html', '/') + tag.attrs['href'])
+            pdf_papers.append(baseURL + tag.attrs['href'])
+            download_file(baseURL.replace('.html', '/') + tag.attrs['href'], tag.attrs['href'].replace('/', ''))
+    return pdf_papers, count
+  
+
+pdfs, count1 = get_pdf(links)
+
+
+
+
+endings = ['ANN', 'BIOMED', 'DAT', 'DIAL', 'FSM', 'GEN', 'HAN', 'HUM', 'LEX', 'MEDIA', 'MOL', 'MT', 'NLL', 'PARSE', 'MORPHON', 'SEM', 'SLAV', 'SEMITIC', 'SLPAT', 'UR', 'WAC']
+endings = ['sig' + item.lower() for item in endings]
+
+links = []
+for ending in endings:
+    link = url + ending + '.html'
+    links.append(link)
+pdfs, count2 = get_pdf(links, url)
+
+
+print('number of pdf files: ', count1, count2, count1 + count2)
---
+++ Scraper/test.py
@@ -0,0 +1,138 @@
+import json
+import re
+from bs4 import BeautifulSoup
+import os
+import glob
+
+
+
+path = ".\\data\LREC\\"
+
+
+for year in [2010, 2012, 2014, 2018]:
+    tmp_path = path + 'LREC{}_Proceedings'.format(year)
+
+
+    pdf_files = glob.glob("/home/adam/*.txt")
+
+
+path = ".\\data\LREC\\LREC2016_Proceedings\\summaries\\19.html"
+
+html = open(path, 'r').read()
+soup = BeautifulSoup(html, , 'html.parser')
+
+soup.find('a', href=)
+
+
+
+data = {
+'author': 'Philip Kurzendörfer',
+'year' : 2019,
+'number' : 10,
+'title':'dies ist ein sehr sinnloser titel',
+'abstract': 'Alles was gut ist',
+'topics': ['topic1', 'topic 2', 'topic 3'],
+'keywords': ['kw1', 'kw2', 'kw3'],
+'fulltext': 'dies ist ein sehr kurzer beispiel text ohne nennenswerten inhalt oder sinn sondern einfach sinnlos aneinandergekettete wörter die man problemlos auch ersetzen kann durch andere wörter die dem text mehr sinn geben würden',
+}
+
+
+with open('data.json', 'w') as fp:
+    json.dump(data, fp)
+
+
+
+
+
+import io
+
+from pdfminer.converter import TextConverter
+
+from pdfminer.pdfinterp import PDFPageInterpreter
+
+from pdfminer.pdfinterp import PDFResourceManager
+
+from pdfminer.pdfpage import PDFPage
+
+def extract_text_from_pdf(pdf_path):
+
+    resource_manager = PDFResourceManager()
+
+    fake_file_handle = io.StringIO()
+
+    converter = TextConverter(resource_manager, fake_file_handle)
+
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(pdf_path, 'rb') as fh:
+
+        for page in PDFPage.get_pages(fh, 
+
+                                      caching=True,
+
+                                      check_extractable=True):
+
+            page_interpreter.process_page(page)
+
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+
+    converter.close()
+
+    fake_file_handle.close()
+
+    if text:
+
+        return text
+
+
+
+def extract_text_by_page(pdf_path):
+
+    with open(pdf_path, 'rb') as fh:
+
+        for page in PDFPage.get_pages(fh, 
+
+                                      caching=True,
+
+                                      check_extractable=True):
+
+            resource_manager = PDFResourceManager()
+
+            fake_file_handle = io.StringIO()
+
+            converter = TextConverter(resource_manager, fake_file_handle)
+
+            page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+            page_interpreter.process_page(page)
+
+            text = fake_file_handle.getvalue()
+
+            yield text
+
+            # close open handles
+
+            converter.close()
+
+            fake_file_handle.close()
+
+def extract_text(pdf_path):
+
+    for page in extract_text_by_page(pdf_path):
+
+        print(page)
+
+        print()
+
+# if __name__ == '__main__':
+
+#     print(extract_text(".\\data\LREC\\LREC2012_Proceedings\\pdf\\194_Paper.pdf"))
+
+
+
+
+if __name__ == '__main__':
+
+    print(extract_text_from_pdf(".\\data\LREC\\LREC2012_Proceedings\\pdf\\194_Paper.pdf"))
